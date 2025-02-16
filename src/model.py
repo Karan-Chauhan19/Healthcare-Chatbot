63f8c5643ce92b7b70b58f59909d0dfa662b5767
karan190806 <kc879022@gmail.com>
2025-02-16 15:32:51 +0530

    added model.py

---
+++ src/model.py
@@ -0,0 +1,14 @@
+from langchain_community.llms import HuggingFaceHub
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+hf = HuggingFaceHub(
+    repo_id="mistralai/Mistral-7B-v0.1",
+    model_kwargs={"temperature":0.1,"max_length":500}
+)
